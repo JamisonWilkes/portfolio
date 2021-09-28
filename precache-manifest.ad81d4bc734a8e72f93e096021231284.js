a7da2b80a2ed6c31cab097ccdb0ccb44f2c69515
Jamison Wilkes <60168055+JamisonWilkes@users.noreply.github.com>
2021-09-27 19:58:20 -0600

    Updates

---
+++ precache-manifest.ad81d4bc734a8e72f93e096021231284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cd65d9a82ca5ba9e10e9a4b26f15e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e9193822d2f1313f6800",
+    "url": "/portfolio/static/css/main.2afd1f1b.chunk.css"
+  },
+  {
+    "revision": "97c5a43dfad28f552d62",
+    "url": "/portfolio/static/js/2.5bf6a433.chunk.js"
+  },
+  {
+    "revision": "e9193822d2f1313f6800",
+    "url": "/portfolio/static/js/main.b2c5ba86.chunk.js"
+  },
+  {
+    "revision": "87b8676f3a5f2a732810",
+    "url": "/portfolio/static/js/runtime-main.fc8ce58b.js"
+  }
+]);
